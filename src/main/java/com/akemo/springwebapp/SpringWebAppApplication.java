bb4ce4e85470a32459168c35e84e0c6585f7c439
Павел Хапёрский <kayan3000@yandex.ru>
2019-09-17 22:04:14 +0300

    Initial commit

---
+++ src/main/java/com/akemo/springwebapp/SpringWebAppApplication.java
@@ -0,0 +1,9 @@
+package com.akemo.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebAppApplication.class,args);
+    }
+}
---
+++ src/main/java/com/akemo/springwebapp/repositories/ProductRepository.java
@@ -0,0 +1,36 @@
+package com.akemo.springwebapp.repositories;
+
+
+import com.akemo.springwebapp.entities.Product;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+    private List<Product> products;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>();
+        products.add(new Product(1L, "Milk", 80));
+        products.add(new Product(2L, "Cheese", 320));
+        products.add(new Product(3L, "Ball", 200));
+    }
+
+    public void deleteById(Long id) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId().equals(id)) {
+                products.remove(i);
+                return;
+            }
+        }
+    }
+}
+
